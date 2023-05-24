fdb5c74fa55e91538051ced410a51503b077977c
saurabh <waghmodesaurabh@gmail.com>
2023-05-24 23:48:18 +0530

    basic css,feat like button state changes

---
+++ xflix/src/Components/Header/Header.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react"
+import "./Header.css"
+
+const genre = [
+    "All Genre",
+    "Education",
+    "Sports",
+    "Comedy",
+    "Lifestyle",
+
+    // {
+    //     0: "All Genre",
+    // },
+    // {
+    //     1: "Education",
+    // },
+    // {
+    //     2: "Sports",
+    // },
+    // {
+    //     3: "Comedy",
+    // },
+    // {
+    //     4: "LifeStyle",
+    // }
+]
+
+const AgeFilters = [
+    "Any age group",
+    "7+",
+    "12+",
+    "16+",
+    "18+",
+]
+const Header = () => {
+    const [Genre, setGenre] = useState(genre[0]);
+    const [Age, setAge] = useState(AgeFilters[0]);
+
+    console.log("age", Age);
+    console.log("agefilter", AgeFilters[0]);
+
+
+    const handleGenre = (id) => {
+        setGenre(genre[id])
+    }
+    const handleAge = (id) => {
+        setAge(AgeFilters[id])
+    }
+    return (
+        <div className="upper-header">
+            <div className="header">
+                <span className="logo">
+                    <span>X</span>
+                    Flix
+                </span>
+                <span className="searchBar">
+                    <input type="text" className='searchBar-input' placeholder="Search" />
+                    <button className="searchBar-button">S</button>
+                </span>
+                <span className="profile">
+                    <span>I</span>
+                    Upload
+                </span>
+            </div>
+            <div className="genre">
+                <div className="genre-chips">
+                    {genre.map((item, id) =>
+                        <span className={Genre === item ? "chip Active" : "chip"} key={id}
+                        onClick={()=>handleGenre(id)}>
+                            {item}
+                        </span>
+                    )}
+                </div>
+                {/* <span>Release Date</span>    */}
+            </div>
+            <div className="filters">
+                {AgeFilters.map((item, id) =>
+                    < span key={id} className={Age === item ? "filterChip Active" : "filterChip"}
+                        onClick={() => handleAge(id)}
+                    >
+                        {item}
+                    </span>
+                )}
+            </div>
+        </div >
+    )
+}
+
+export default Header
---
+++ xflix/src/Pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Dashboard.css";
+import Header from '../Components/Header/Header';
+
+const Dashboard = () => {
+    return (
+        <div className="dashboard">
+            <Header />
+        </div>
+    )
+}
+
+export default Dashboard
