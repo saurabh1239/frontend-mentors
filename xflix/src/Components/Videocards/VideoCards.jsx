28f06cd80f18fc2f805cec310a5a436c50984597
saurabh <waghmodesaurabh@gmail.com>
2023-05-29 23:50:39 +0530

    last

--- xflix/src/Components/Videocards/VideoCards.jsx
+++ xflix/src/Components/Videocards/VideoCards.jsx
@@ -1,19 +1,7 @@
 import { Link, useLocation } from "react-router-dom";
 import "./VideoCards.css"
-// "votes": {
-//     "upVotes": 0,
-//     "downVotes": 0
-// },
-// "previewImage": "https://i.ytimg.com/vi/nx2-4l4s4Nw/mqdefault.jpg",
-// "viewCount": 83,
-// "_id": "60331f421f1d093ab5424489",
-// "videoLink": "youtube.com/embed/nx2-4l4s4Nw",
-// "title": "Consumed by the Apocalypse",
-// "genre": "Movies",
-// "contentRating": "12+",
-// "releaseDate": "18 Jan 2021"
+
 const VideoCards = ({ videos, propData, setPropData }) => {
-    // console.log("vider",typeof(videos));
     const params = useLocation();
 
     const handleVideo = (link) => {
