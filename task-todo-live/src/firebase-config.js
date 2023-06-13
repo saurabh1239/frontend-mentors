09f471ea23e0c03a210f94eb3b90160c55705c33
saurabh <waghmodesaurabh@gmail.com>
2023-06-13 22:40:20 +0530

    adding firebase

---
+++ task-todo-live/src/firebase-config.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyAX1p9vtKIo3mCFPU06wsXOBsJhch-vpIE",
+  
+    authDomain: "task-todo-live.firebaseapp.com",
+  
+    projectId: "task-todo-live",
+  
+    storageBucket: "task-todo-live.appspot.com",
+  
+    messagingSenderId: "422139621915",
+  
+    appId: "1:422139621915:web:dd404070cc3601cdcbfebe",
+  
+    measurementId: "G-TD83DSBZQX"
+  
+  };
+  
+  
+  // Initialize Firebase
+  
+  const app = initializeApp(firebaseConfig);
+  
+//   const analytics = getAnalytics(app);
