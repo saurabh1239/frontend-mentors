618d218f83a223b4d7c525c77bcbb1c6315f93d4
saurabh <waghmodesaurabh@gmail.com>
2023-06-15 23:06:03 +0530

    added feat for multiple edition and deletion, feat distinguished todo categories

--- task-todo-live/src/App.jsx
+++ task-todo-live/src/App.jsx
@@ -1,53 +1,102 @@
 import { useEffect, useState } from 'react'
-import { db } from './firebase-config'
-import { collection, getDocs, addDoc, updateDoc, doc, deleteDoc } from "firebase/firestore"
-import "./index.css"
+import { v4 as uuidv4 } from "uuid";
+import bg_dark from "../public/images/bg-desktop-dark.jpg"
+import "./app.css"
 function App() {
-  const [Name, setName] = useState("");
-  const [Age, setAge] = useState(0);
-  const [users, setUsers] = useState([])
-  const userCollectionRef = collection(db, "users")
+  const [Task, setTask] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const [checked, setChecked] = useState([])
+  // const userCollectionRef = collection(db, "users")
 
 
-  const handleCreate = async () => {
-    await addDoc(userCollectionRef, { name: Name, age: Age })
-  }
-
-  const editAge = async (id,age) => {
-    const userDoc = doc(db, "users", id);
-    const newfields = { age: age + 1 };
-    await updateDoc(userDoc, newfields)
-  }
-  const handleDelete = async(id)=>{
-    const userDoc = doc(db,"users",id)
-    await deleteDoc(userDoc)
-  }
-  useEffect(() => {
-    const getUsers = async () => {
-      const data = await getDocs(userCollectionRef);
-      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+  const addTOdod = (todo) => {
+    if (Task) {
+      setTodoList([...todoList, { id: uuidv4(), Task: todo, checked: false }])
+      setTask("")
     }
-    getUsers();
-  }, [users])
 
+  }
+  const handleTodo = (e) => {
+    e.preventDefault();
+    addTOdod(Task)
+  }
+
+  const handleCheck = (Id) => {
+    setTodoList(
+      todoList.map((todo) =>
+        todo.id === Id ? { ...todo, checked: !todo.checked } : todo
+      )
+    );
+    setChecked(todoList.map((todo) =>
+      todo.checked == true ? { id: todo.id, checked: todo.checked } : checked.filter((todo) => todo.checked === false)
+    ))
+  }
+
+  const clearChecked = () => {
+    setTodoList(todoList.filter((item) => item.checked === false)
+    )
+    console.log(checked);
+  }
   return (
     <div className='app'>
-      <input type="text" placeholder='name' value={Name} onChange={(e) => setName(e.target.value)} />
-      <input type="number" placeholder='age' value={Age} onChange={(e) => setAge(e.target.value)} />
-      <button onClick={handleCreate}>create</button>
-      {
-        users.map((item) =>
-          <div key={item.id} className='name'>
-            <h1>{item.name}</h1>
-            <h1>{item.age}</h1>
-            <button onClick={() => editAge(item.id,item.age)}>Edit Age</button>
-            <button onClick={() => handleDelete(item.id)}>delete</button>
-            {/* <h1>{item.email}</h1> */}
-          </div>
-        )
-      }
+      <div className="uppernav">
+      </div>
+      <div className="dialog-box">
+        <div className="navbar">
+          <h1>TODO</h1>
+          <button>dark</button>
+        </div>
+
+        <form onSubmit={handleTodo} className='todo-input-wrapper'>
+          <input type="text" placeholder='name' value={Task} onChange={(e) => setTask(e.target.value)} className='todo_input' />
+          <button >s</button>
+        </form>
+
+        <ul className="todo-list">
+          {
+            todoList.map((item) =>
+              <li key={item.id} className='task'>
+                <input type="checkbox" checked={item.checked} onChange={() => handleCheck(item.id)} />
+                <p>{item.Task}</p>
+              </li>
+            )
+          }
+        </ul>
+        <div className="dialog-footer">
+          <div>{todoList.length ? todoList.length : 0} items left</div>
+          <ul className='footer-item-ul'>
+            <li>All</li>
+            <li>Active</li>
+            <li>Completed</li>
+          </ul>
+          <button onClick={() => clearChecked()}>clear completed</button>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default App
+export default App;
+
+
+
+// const handleCreate = async () => {
+//   await addDoc(userCollectionRef, { name: Name, age: Age })
+// }
+
+// const editAge = async (id,age) => {
+//   const userDoc = doc(db, "users", id);
+//   const newfields = { age: age + 1 };
+//   await updateDoc(userDoc, newfields)
+// }
+// const handleDelete = async(id)=>{
+//   const userDoc = doc(db,"users",id)
+//   await deleteDoc(userDoc)
+// }
+// useEffect(() => {
+//   const getUsers = async () => {
+//     const data = await getDocs(userCollectionRef);
+//     setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+//   }
+//   getUsers();
+// }, [users])
